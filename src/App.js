c4ddd1317cbaa2e026b8ebe998ece0eb5818c033
SUJAY GOWA N <1sujay1>
2023-09-26 18:43:55 +0530

    Login and Autendication done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react"
+import Login from "./components/Login";
 
-const App = (props) => {
+const App = () => {
   return (
     <div>
-      Helllo
+      <Login/>
     </div>
   )
 };
